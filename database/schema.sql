ceabfdbb0f7db6cc643294176bee4fa17973135c
Tomás Sarmento <up202108778@fe.up.pt>
2024-11-07 14:08:15 +0000

    User interface initial

--- database/schema.sql
+++ database/schema.sql
@@ -1,14 +1,8 @@
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-);
 
 CREATE TABLE shoppingList (
     id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
-    creator INTEGER NOT NULL,
-    FOREIGN KEY (creator) REFERENCES users(id)
+    creator INTEGER NOT NULL
 );
 
 CREATE TABLE item (
@@ -16,6 +10,7 @@ CREATE TABLE item (
     name TEXT NOT NULL,
     list_id INTEGER NOT NULL,
     quantity INTEGER DEFAULT 1,
+    deleted BOOLEAN DEFAULT FALSE,
     FOREIGN KEY (list_id) REFERENCES shoppingList(id),
     quantity INTEGER DEFAULT 1
 );
