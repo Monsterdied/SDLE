2c0da49ad1c8377ae0d9571fb992bfb93cc69fd3
Tomás Sarmento <up202108778@fe.up.pt>
2024-12-07 16:28:04 +0000

    implemented mutexes

--- src/test/consistent_hash.js
+++ src/test/consistent_hash.js
@@ -1,32 +1,42 @@
 const crypto = require('crypto');
+const { Mutex } = require('async-mutex');
+const ReadwriteLock = require('readwrite-lock');
+//TODO
 class ConsistentHash {
     constructor(replicas = 100) {
         this.replicas = replicas;
         this.ring = new Map();
         this.nodes = new Set();
+        this.lock = new Mutex();
     }
+    safeRead() {
 
+    }
     addNode(node) {
+        this.lock.acquire();
         this.nodes.add(node);
         // Add virtual nodes
         for (let i = 0; i < this.replicas; i++) {
             const hash = this.getHash(`${node}:${i}`);
             this.ring.set(hash, node);
         }
+        this.lock.release();
     }
 
     removeNode(node) {
+        this.lock.acquire();
         this.nodes.delete(node);
         // Remove virtual nodes
         for (let i = 0; i < this.replicas; i++) {
             const hash = this.getHash(`${node}:${i}`);
             this.ring.delete(hash);
         }
+        this.lock.release();
     }
 
     getNode(key) {
         if (this.ring.size === 0) return null;
-
+        this.lock.acquire();
         const hash = this.getHash(key);
         const sortedHashes = Array.from(this.ring.keys()).sort();
         //sortedHashes.forEach((h) => console.log(h));
@@ -34,9 +44,9 @@ class ConsistentHash {
         for (const h of sortedHashes) {
             if (h >= hash) return this.ring.get(h);
         }
-        
-        // Wrap around to first node if we're past the end
-        return this.ring.get(sortedHashes[0]);
+        const result = this.ring.get(sortedHashes[0]);
+        this.lock.release();
+        return result;
     }
 
     getHash(key) {
